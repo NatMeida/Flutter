870d25d49477397b5d74352064afbb520f3c2d21
NaCarvalho <cl201306@g.unicamp.br>
2023-08-21 15:41:52 -0300

    Tela
    Tela de login pronta sem validação

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,10 @@ class MyApp extends StatelessWidget {
                 child: Padding(
                   padding: EdgeInsets.all(18),
                   child: Column(children: [
+                    DecoratedBox(decoration: BoxDecoration(color: Color.fromARGB(255, 255, 254, 254),), child: SizedBox(width: 300, height: 50, child: Center(child: Text("Login", style: TextStyle(color: Colors.deepOrange, fontSize: 30, fontWeight: FontWeight.bold),),),),),
+                    SizedBox(height: 10,),
+                    DecoratedBox(decoration: BoxDecoration(color: Color.fromARGB(255, 90, 43, 100),), child: SizedBox(width: 500, height: 5, ),),
+                    SizedBox(height: 20,),
                     TextFormField(
                       //controller: campoUsuario,
                       validator: (value) {
@@ -47,13 +51,15 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
 
+                    SizedBox(height: 20,),
+
                     TextFormField(
                       //controller: campoUsuario,
                       validator: (value) {
                          if (value!.isEmpty){
                           return ("Insira a senha para efetuar login.");
                          } else {
-                          if (campoSenha.text.length < 4){
+                          if (9 < 4){
                             return ("A senha deve conter ao menos 4 caracteres");
                           }
                          }
@@ -68,7 +74,15 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
 
-                  ]),
+                    SizedBox(height: 20,),
+                    
+                    ElevatedButton(onPressed: (){
+                      
+                    },
+                     child: Text("Entrar", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+                    )
+                  ]
+                  ),
                   )
                   )
 
