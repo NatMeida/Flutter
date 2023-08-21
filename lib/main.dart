cb7e3bcf5f2269db7a822e1768315f05975623f7
NaCarvalho <cl201306@g.unicamp.br>
2023-08-21 11:01:19 -0300

    odeio flutter

--- lib/main.dart
+++ lib/main.dart
@@ -12,56 +12,67 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter Demo Home Page'),
+          title: const Text('Exercício - Login com Validação'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-              Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-              Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-              Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-              Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
+            children: [
+              Image.network("https://cdn4.iconfinder.com/data/icons/essentials-74/24/008_-_Lock-512.png", width: 210, height: 210,),
+              SizedBox(height: 20,),
+
+              Form(
+                key: key,
+                child: Padding(
+                  padding: EdgeInsets.all(18),
+                  child: Column(children: [
+                    TextFormField(
+                      //controller: campoUsuario,
+                      validator: (value) {
+                         if (value!.isEmpty){
+                          return ("Insira o nome de usuário para efetuar login.");
+                         }
+                         return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Usuário",
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+
+                    TextFormField(
+                      //controller: campoUsuario,
+                      validator: (value) {
+                         if (value!.isEmpty){
+                          return ("Insira a senha para efetuar login.");
+                         } else {
+                          if (campoSenha.text.length < 4){
+                            return ("A senha deve conter ao menos 4 caracteres");
+                          }
+                         }
+                         return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        prefixIcon: Icon(Icons.key_rounded),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+
+                  ]),
+                  )
+                  )
+
+
             ],
           )
         )
